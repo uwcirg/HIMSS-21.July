226cc7015480a855b4e125c37db7305752f35e3c
Paul Bugni <pbugni@u.washington.edu>
2021-07-22 17:08:39 -0700

    initial stab at simple model class for housing patient details from Mirth's eICR channels.

---
+++ backend/backend_app/api/models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+from backend.backend_app.db import db
+
+
+class Patient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    simple_xml = db.Column(db.Text, nullable=True)
+    eicr_html = db.Column(db.Text, nullable=True)
+    rr_html = db.Column(db.Text, nullable=True)
+
+    def __repr__(self):
+        return "<Patient %d>" % self.id
