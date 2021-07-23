b17f2858269bcf2b29a3194f56bee2ca154b6a5c
Paul Bugni <pbugni@u.washington.edu>
2021-07-22 17:57:12 -0700

    connect to docker's db network

--- backend/backend_app/config.py
+++ backend/backend_app/config.py
@@ -9,5 +9,5 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # URL scheme to use outside of request context
 PREFERRED_URL_SCHEME = os.getenv("PREFERRED_URL_SCHEME", 'http')
 SQLALCHEMY_DATABASE_URI = os.getenv(
-    "SQLALCHEMY_DATABASE_URI", "postgresql://mirthdb@mirthdb/mirthdb")
+    "SQLALCHEMY_DATABASE_URI", "postgresql://mirthdb:mirthdb@db/mirthdb")
 SQLALCHEMY_TRACK_MODIFICATIONS = False  # Quiet warning
