e9cbbc4c55dd840f67da198378882cb11b581f4d
Paul Bugni <pbugni@u.washington.edu>
2021-07-22 22:16:27 -0700

    add some test data and basic query view

--- backend/backend_app/api/views.py
+++ backend/backend_app/api/views.py
@@ -1,6 +1,9 @@
 from flask import Blueprint
+from ..db import db
+from .models import Patient
 
 base_blueprint = Blueprint('base', __name__)
+
 
 @base_blueprint.route('/')
 def root():
@@ -9,6 +12,26 @@ def root():
 
 @base_blueprint.route('/init-db')
 def init_db():
-    from backend_app.db import db
     db.create_all()
     return {'ok': True}
+
+
+@base_blueprint.route('/dummy')
+def dummy():
+    p = Patient()
+    p.simple_xml = "<xml><not>really</not></xml>"
+
+    db.session.add(p)
+    db.session.commit()
+    return {'added': p}
+
+
+@base_blueprint.route('/Patient')
+def patient_list():
+    patients = []
+    for p in Patient.query():
+        patients.append({
+            'id': p.id,
+            'xml': p.simple_xml
+        })
+    return {'patients': patients}
