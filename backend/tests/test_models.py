d602205821983e53b29b3d5248b99a185e437496
pbugni <pbugni@u.washington.edu>
2021-07-27 15:58:01 -0700

    Added several additional attributes as per request. (#5)
    Extended model to hold `reportable.xml` as Mirth needs to write a second XML snippet to enable parsing the `reportable` condition.

--- backend/tests/test_models.py
+++ backend/tests/test_models.py
@@ -11,10 +11,27 @@ def simple_xml(datadir):
     return data
 
 
-def test_parse_names(simple_xml):
+@pytest.fixture
+def reportable_xml(datadir):
+    with open(os.path.join(datadir, 'reportable.xml'), 'r') as xml_file:
+        data = xml_file.read()
+    return data
+
+
+def test_parse_names(simple_xml, reportable_xml):
     patient = Patient()
     patient.simple_xml = simple_xml
+    patient.reportable_xml = reportable_xml
     results = patient.json()
     assert results['first_name'] == 'Testy'
     assert results['last_name'] == 'McPatient'
     assert results['gender'] == 'M'
+    assert results['birthdate'] == '19990101'
+    assert results['address'] == 'UNK'
+    assert results['city'] == 'UNK'
+    assert results['state'] == 'UNK'
+    assert results['zip'] == 'UNK'
+    assert results['race'] == 'UNK'
+    assert results['ethnicity'] == 'UNK'
+    assert results['reportable_conditions'] == [
+        'Disease caused by severe acute respiratory syndrome coronavirus 2 (disorder)']
